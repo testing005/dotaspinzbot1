592741eab086f0cdfd0bee67be24cd4c29a66c02
testing005 <xzinehacker@gmail.com>
2017-07-22 18:30:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/fb",function(req,res){
 	var title=req.query.title;
 	var img=req.query.img;
 
-	res.writeHead(200,{"Content-Type":"text/html"});
+	/*res.writeHead(200,{"Content-Type":"text/html"});
 	res.write(" "+"<meta property=\"og:site_name\" content=\"Youtube\">");
 	//res.write(" "+"<meta property=\"og:url\" content=\"https://www.youtube.com/watch?v="+id+"\">");
 	res.write(" "+"<meta property=\"og:title\" content=\""+title+"\">");
@@ -40,8 +40,10 @@ app.get("/fb",function(req,res){
 	res.write(" "+"<meta property=\"og:video:type\" content=\"application/x-shockwave-flash\">");
 	res.write(" "+"<meta property=\"og:video:width\" content=\"640\">");
 	res.write(" "+"<meta property=\"og:video:height\" content=\"360\">");
-    res.end();
-
+    res.end();*/
+res.writeHead(200,{"Content-Type":"text/html"});
+res.write(" "+"<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><meta name=\"description\" content=\"Magic. ➔ Télécharge &quot;OneFootball&quot; GRATUITEMENT : http://bit.do/Aliotop_May ➔ Maillots de Foot 2017/2018 à 20 € : http://maxmaillots.org/?utm_source=aliotop ►...\"><meta name=\"image\" content=\"http://img.youtube.com/vi/ZrHaCSJtttc/0.jpg\"><link rel=\"image_src\" href=\"http://img.youtube.com/vi/ZrHaCSJtttc/0.jpg\"><meta property=\"og:image\" content=\"http://img.youtube.com/vi/ZrHaCSJtttc/0.jpg\"><meta property=\"og:title\" content=\'Lionel Messi - God of Football (HD)\'><meta property=\"og:description\" content=\"Magic. ➔ Télécharge &quot;OneFootball&quot; GRATUITEMENT : http://bit.do/Aliotop_May ➔ Maillots de Foot 2017/2018 à 20 € : http://maxmaillots.org/?utm_source=aliotop ►...\"><!--meta property=\'og:url\' content=\'https://www.youtube.com/embed/ZrHaCSJtttc?start=0&amp;autoplay=1\' /--><meta property=\"og:video\" content=\"https://www.youtube.com/embed/ZrHaCSJtttc?start=0&amp;autoplay=1\"><meta property=\"og:video:type\" content=\"application/x-shockwave-flash\"><meta property=\"og:video:width\" content=\"1280\"><meta property=\"og:video:height\" content=\"720\"><meta property=\"og:video\" content=\"https://www.youtube.com/embed/ZrHaCSJtttc?start=0&amp;autoplay=1\"><meta property=\"og:video:secure_url\" content=\"https://www.youtube.com/embed/ZrHaCSJtttc?start=0&amp;autoplay=1\"><meta property=\"og:video:type\" content=\"text/html\"><meta property=\"og:video:width\" content=\"1280\"><meta property=\"og:video:height\" content=\"720\"><meta property=\"og:type\" content=\"article\"><meta property=\"article:author\" content=\"https://www.facebook.com/youtube/\"><meta property=\"og:audio\" content=\"https://www.youtube.com/embed/ZrHaCSJtttc?list=&amp;autoplay=1\"><meta property=\"og:audio:type\" content=\"application/x-shockwave-flash\"><link itemprop=\"embedURL\" href=\"https://www.youtube.com/embed/ZrHaCSJtttc?list=\">");
+res.end();
 
 }
 	)
